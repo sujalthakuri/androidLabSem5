69a9179740090541d5c9ceac713bd5d95d63c2ef
Sujal <you@gmail.com>
2020-12-27 10:53:26 +0545

    Person Class Added

--- app/src/main/java/com/example/sujal/formActivity.kt
+++ app/src/main/java/com/example/sujal/formActivity.kt
@@ -2,11 +2,46 @@ package com.example.sujal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class formActivity : AppCompatActivity() {
+    private lateinit var etName : EditText
+    private lateinit var etAddress : EditText
+    private lateinit var etMobile : EditText
+    private lateinit var rMale : RadioButton
+    private lateinit var rFemale : RadioButton
+    private lateinit var btnSumit : Button
+    var gender : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+        retrieveValues()
+        rMale.setOnClickListener{
+            gender = "male"
+        }
+
+        rFemale.setOnClickListener{
+            gender = "female"
+        }
+
+        btnSumit.setOnClickListener{
+            Toast.makeText(this,"$gender",Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+    }
+
+    private fun retrieveValues()
+    {
+        etName = findViewById(R.id.etName)
+        etAddress = findViewById(R.id.etAddress)
+        etMobile = findViewById(R.id.etMobile)
+        rMale = findViewById(R.id.rMale)
+        rFemale = findViewById(R.id.rFemale)
+        btnSumit = findViewById(R.id.btnSubmit)
     }
 }
